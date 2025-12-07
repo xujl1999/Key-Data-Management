b5051940f9e5f0f597408198037cae8df04827f1
xujl1999 <xujl1999@users.noreply.github.com>
2025-12-07 21:57:57 +0800

    test

--- video_update.bat
+++ video_update.bat
@@ -1,12 +1,13 @@
 D:
 call C:\Users\23711\Miniconda3\Scripts\activate.bat C:\Users\23711\Miniconda3
-call conda activate base
+call conda activate ba
+c
 cd D:\dream_life\data-management\video
 python get_video_ls.py
-cd D:\dream_life\data-management\web
+cd D:\dream_life\data-management
 git branch
-git pull origin master
+git pull origin main
 git add .
 git commit -m "test"
-git push origin master
+git push origin main
 pause

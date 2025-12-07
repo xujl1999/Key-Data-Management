2d0cd4bc084a1a72c05510393e3816548b30eb16
xujl1999 <xujl1999@users.noreply.github.com>
2025-12-07 21:54:49 +0800

    test

--- video_update.bat
+++ video_update.bat
@@ -1,12 +1,12 @@
 D:
 call C:\Users\23711\Miniconda3\Scripts\activate.bat C:\Users\23711\Miniconda3
 call conda activate base
-cd /d D:\dream_life\data-management\video
+cd D:\dream_life\data-management\video
 python get_video_ls.py
-cd /d D:\dream_life\data-management
+cd D:\dream_life\data-management\web
 git branch
-git pull origin main
+git pull origin master
 git add .
-git commit -m "auto: update data and web"
-git push origin main
+git commit -m "test"
+git push origin master
 pause

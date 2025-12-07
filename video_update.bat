c71130c3c819e1b02665f23650a332d01ae7abdf
xujl1999 <xujl1999@users.noreply.github.com>
2025-12-07 22:40:15 +0800

    test

--- video_update.bat
+++ video_update.bat
@@ -1,7 +1,6 @@
 D:
 call C:\Users\23711\Miniconda3\Scripts\activate.bat C:\Users\23711\Miniconda3
-call conda activate ba
-c
+call conda activate base
 cd D:\dream_life\data-management\video
 python get_video_ls.py
 cd D:\dream_life\data-management
